593fbefe9015d385932ef60b6f83e2c8aa82cbfa
Rox-C <2731107028@qq.com>
2025-10-04 20:11:26 +0800

    MariaDB

--- include/utils/MysqlUtil.h
+++ include/utils/MysqlUtil.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "db/DbConnectionPool.h"
- 
+#include <mysql/mysql.h>  // 引入MariaDB头文件
 #include <string>
 
 namespace http
@@ -9,6 +9,7 @@ namespace http
 class MysqlUtil
 {
 public:
+    // 初始化数据库连接池
     static void init(const std::string& host, const std::string& user,
                     const std::string& password, const std::string& database,
                     size_t poolSize = 10)
@@ -17,13 +18,15 @@ public:
             host, user, password, database, poolSize);
     }
 
+    // 执行查询操作，返回MariaDB原生结果集指针
     template<typename... Args>
-    sql::ResultSet* executeQuery(const std::string& sql, Args&&... args)
+    MYSQL_RES* executeQuery(const std::string& sql, Args&&... args)
     {
         auto conn = http::db::DbConnectionPool::getInstance().getConnection();
         return conn->executeQuery(sql, std::forward<Args>(args)...);
     }
 
+    // 执行更新操作（INSERT/UPDATE/DELETE），返回受影响的行数
     template<typename... Args>
     int executeUpdate(const std::string& sql, Args&&... args)
     {
